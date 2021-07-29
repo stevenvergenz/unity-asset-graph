14718c9c93df573be411de727e046f2a7a5f19d7
Steven Vergenz <vergenzs@gmail.com>
2021-07-28 22:57:30 -0700

    Output something!

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,36 @@
-import { Database } from './database';
+import { exec } from 'child_process';
+import { writeFile } from 'fs/promises';
+
+import { Database } from './database.js';
+import { visualize } from './visualize.js';
 
 async function main(argv: string[]): Promise<void> {
-	const command = argv[0];
+	const [command, arg1, arg2, arg3] = argv;
 
 	if (command === "extract"){
 		const db = new Database();
-		await db.extract(argv[1]);
-		await db.save(argv[2]);
+		await db.extract(arg1);
+		await db.save(arg2);
 	}
 	else if (command === "query") {
 		const db = new Database();
-		await db.load(argv[1]);
+		await db.load(arg1);
 
-		const subc = argv[2];
+		const subc = arg2;
 		if (subc == "dependencies") {
-			console.log(db.formatDependencies(db.findByName(argv[3])));
+			console.log(db.formatDependencies(db.findByName(arg3)));
 		}
+	}
+	else if (command === "visualize") {
+		const db = new Database();
+		await db.load(arg1)
+
+		const svg = visualize(db, 1920, 1080);
+		await writeFile(arg2, svg.html());
+		exec(`open "${arg2}"`);
 	}
 }
 
-const argBase = process.argv.indexOf(__filename);
-main(process.argv.slice(argBase + 1))
+const argBase = process.argv.findIndex(arg => /extract|query|visualize/.test(arg));
+main(process.argv.slice(argBase))
 .catch(err => console.error(err));
--- src/visualize.ts
+++ src/visualize.ts
@@ -1,18 +1,44 @@
 import * as d3 from 'd3';
 import * as d3f from 'd3-force';
-import { Asset, Database } from './database';
+import { JSDOM } from 'jsdom';
 
-export function visualize(db: Database, width = 1920, height = 1080): SVGSVGElement {
+import { Asset, Database } from './database.js';
+
+export function visualize(db: Database, width = 1920, height = 1080) {
 	const data = generateSimData(db);
 	const sim = d3f.forceSimulation(data.nodes)
 		.force("links", d3f.forceLink(data.links).id((a: Asset) => a.id))
 		.force("charge", d3f.forceManyBody())
-		.force("center", d3f.forceCenter(width / 2, height / 2));
+		.force("center", d3f.forceCenter(width / 2, height / 2))
+		.stop();
 
-	const svg = d3.create("svg")
+	for (let i = 0; i < 20; i++) {
+		sim.tick();
+	}
+
+	const doc = new JSDOM("<body></body>");
+	const body = d3.select(doc.window.document.body)
+	const svg = body.append("svg")
 		.attr("viewBox", `0 0 ${width} ${height}`);
 
-	return svg.node();
+	svg.append("g")
+		.attr("stroke", "#999")
+		.attr("stroke-opacity", 0.6)
+		.attr("stroke-width", 2)
+		.selectAll("line")
+		.data(data.links)
+		.join("line");
+
+	svg.append("g")
+		.attr("stroke", "#fff")
+		.attr("stroke-width", 1.5)
+		.selectAll("circle")
+		.data(data.nodes)
+		.join("circle")
+		.attr("r", 5)
+		.attr("fill", "#555");
+
+	return body;
 }
 
 function generateSimData(db: Database): { nodes: Asset[], links: d3f.SimulationLinkDatum<Asset>[] } {
